081d1d04d68380260d588add1c74e10c76e48f2f
JackCui001 <443289353@qq.com>
2019-11-17 17:25:54 +0800

    Add source files

---
+++ A-Set/1146A - Love "A".cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string s;
+	int i,count=0;
+	double len;
+	cin>>s;
+	len=s.size();
+	for(i=0;i<len;i++)
+	{
+		if(s[i]=='a')
+		    count++;
+	}
+	if(count>len/2)
+	{
+		cout<<(int)len;
+	}
+	else
+	{
+		while(len/2>=count)
+		    len--;
+		cout<<len;
+	}
+	return 0;
+}
