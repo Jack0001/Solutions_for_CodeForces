f45856a35dff0409f9a7d3fa93e3c56a08bf3ab1
JackCui001 <443289353@qq.com>
2019-11-18 16:47:51 +0800

    Add source file

---
+++ A-Set/734A - Anton and Danik.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int num,i,A_count=0,D_count=0;
+	cin >> num;
+	string str;
+	cin >> str;
+	for(i=0;i<num;i++)
+	{
+		if(str[i]=='A')
+		    A_count++;
+		if(str[i]=='D')
+		    D_count++;
+	}
+	if(A_count<D_count)
+	    cout << "Danik";
+	else if(A_count>D_count)
+	    cout << "Anton";
+	else if(A_count==D_count)
+	    cout << "Friendship";
+    return 0;
+}
